be5d4d9706f604b9de52493b7eac792a912011cf
Thanushree0499 <73820030+Thanushree0499@users.noreply.github.com>
2020-11-03 10:03:49 +0530

    Add files via upload

---
+++ SquareOfNumber.java
@@ -0,0 +1,8 @@
+class SquareOfNumber{
+	public static void main(String args[]){
+	int num[]={5,7,12,8,98,6,4,56,34,11,10,6,4,};
+	for(int i=1;i<14;i++){
+	System.out.println(num[i]*num[i]);
+	}
+}
+}

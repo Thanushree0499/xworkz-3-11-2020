2153dbedbcbddab261e08547dcffd259035abe6d
Thanushree0499 <73820030+Thanushree0499@users.noreply.github.com>
2020-11-03 10:02:43 +0530

    Add files via upload

---
+++ EvenIndexValues.java
@@ -0,0 +1,10 @@
+class EvenIndexValues{
+	public static void main(String args[]){
+	int values[]={12,5,9,34,67,2,67,89,75};
+	for(int i=0;i<9;i++){
+	if(i%2==0){
+	System.out.println(values[i]);
+	}
+	}
+}
+}

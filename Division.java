cbdc51116393a5bef58aaebc4e0fe19c9c96e9af
Thanushree0499 <73820030+Thanushree0499@users.noreply.github.com>
2020-11-03 10:02:17 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,10 @@
+class Division{
+	public static void main(String args[]){
+	int numbers[]={4,5,9,8,12,6,7,2};
+	for(int i=0;i<8;i++){
+	if(numbers[i]%2==0){
+	System.out.println(numbers[i]);
+	}
+	}
+}
+}
